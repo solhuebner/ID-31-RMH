ef77737578d28779b4861c3ca5f86f625a450544
Joeri Gantois <info@JO3RI.be>
2016-02-22 12:14:22 +0100

    Added files via upload

---
+++ RMH_AB/numbers_bitmaps.h
@@ -0,0 +1,131 @@
+const unsigned char PROGMEM number0_bitmap[] =
+{
+  //    ###
+  //   #  #
+  //  #   #
+  //  #  # 
+  //  #  # 
+  // #   # 
+  // #  #  
+  // ###   
+  0xE0, 0x9C, 0x82, 0x41, 0x39, 0x07, 
+};
+
+const unsigned char PROGMEM number1_bitmap[] =
+{
+  //    ## 
+  //   # # 
+  //     # 
+  //    #  
+  //    #  
+  //    #  
+  //   #   
+  //   #   
+  0x00, 0x00, 0xC2, 0x39, 0x07, 0x00, 
+};
+
+const unsigned char PROGMEM number2_bitmap[] =
+{
+  //    ###
+  //   #  #
+  //  #   #
+  //     # 
+  //    #  
+  //   #   
+  //  #    
+  // ####  
+  0x80, 0xC4, 0xA2, 0x91, 0x09, 0x07, 
+};
+
+const unsigned char PROGMEM number3_bitmap[] =
+{
+  //    ###
+  //   #  #
+  //     ##
+  //    ## 
+  //     # 
+  // #   # 
+  // #  #  
+  // ####  
+  0xE0, 0x80, 0x82, 0xC9, 0x3D, 0x07, 
+};
+
+const unsigned char PROGMEM number4_bitmap[] =
+{
+  //     ##
+  //    ## 
+  //   # # 
+  //   # # 
+  //  # #  
+  // ##### 
+  //    #  
+  //   ##  
+  0x20, 0x30, 0xAC, 0xF2, 0x2F, 0x01, 
+};
+
+const unsigned char PROGMEM number5_bitmap[] =
+{
+  //   ####
+  //   #   
+  //  # ## 
+  //  ## # 
+  //     # 
+  //     # 
+  // ## #  
+  //  ###  
+  0x40, 0xCC, 0x8B, 0xC5, 0x3D, 0x01, 
+};
+
+const unsigned char PROGMEM number6_bitmap[] =
+{
+  //    ###
+  //   #  #
+  //  ##   
+  //  #### 
+  //  #  # 
+  // ##  # 
+  // #  #  
+  // ####  
+  0xE0, 0xBC, 0x8E, 0xC9, 0x39, 0x03, 
+};
+
+const unsigned char PROGMEM number7_bitmap[] =
+{
+  //   ####
+  //      #
+  //     # 
+  //    #  
+  //    #  
+  //   #   
+  //  ##   
+  // ##    
+  0x80, 0xC0, 0x61, 0x19, 0x05, 0x03, 
+};
+
+const unsigned char PROGMEM number8_bitmap[] =
+{
+  //    ###
+  //   #  #
+  //  #  ##
+  //   ### 
+  //  #  # 
+  // #   # 
+  // #  #  
+  // ###   
+  0xE0, 0x94, 0x8A, 0x49, 0x3D, 0x07, 
+};
+
+const unsigned char PROGMEM number9_bitmap[] =
+{
+  //    ###
+  //   #  #
+  //  #   #
+  //  #  # 
+  //  #### 
+  //     # 
+  // #  #  
+  // ####  
+  0xC0, 0x9C, 0x92, 0xD1, 0x39, 0x07, 
+};
+
+const unsigned char *number_bitmaps[] = {number0_bitmap, number1_bitmap, number2_bitmap, number3_bitmap, number4_bitmap, number5_bitmap, number6_bitmap, number7_bitmap, number8_bitmap, number9_bitmap};
